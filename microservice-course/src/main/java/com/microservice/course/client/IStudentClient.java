7860f14d6672d71d8ccbe5aa4c6820931f398875
Hector Rodriguez <hector@hrod.dev>
2023-12-30 13:21:01 -0500

    Implement gateway and fix gateway dependency

--- microservice-course/src/main/java/com/microservice/course/client/IStudentClient.java
+++ microservice-course/src/main/java/com/microservice/course/client/IStudentClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "msvc-student", url = "localhost:8090/api/student")
+@FeignClient(name = "msvc-student", url = "localhost:8080/api/student")
 public interface IStudentClient {
 
     @GetMapping("/search-by-course/{idCourse}")
